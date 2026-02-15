f425e4206aa437f7702e18ca761117f1945eee1c
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-02-15 16:06:33 +0600

    UI: Navbar is updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Navbar from "./Components/Header/Navbar";
+import Navbar from "./Components/layoyt/Navbar";
 
 function App() {
   return (
-    <div className="">
+    <>
       <Navbar />
-    </div>
+    </>
   );
 }
 
