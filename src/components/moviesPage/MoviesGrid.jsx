4b252dca237e098eda71eed16e89af71b3af37c3
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-02-17 15:17:35 +0600

    fix(UI): add padding right (pr-6) in movie row

--- src/components/moviesPage/MoviesGrid.jsx
+++ src/components/moviesPage/MoviesGrid.jsx
@@ -2,7 +2,7 @@ import MovieCard from "../movie/MovieCard";
 import MovieCardSkeleton from "../Skeleton/MovieCardSkeleton";
 export default function MoviesGrid(){
     return (
-        <div className="flex flex-col items-center justify-center py-20 gap-4">
+        <div className="flex flex-col items-center justify-start py-20 gap-4">
             <MovieCard />
             <MovieCard />
             <MovieCard />
