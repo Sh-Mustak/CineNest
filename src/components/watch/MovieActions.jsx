2b7fb340df2161297f1ae5ab9a71a29bd400cb45
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-02-20 15:52:06 +0600

    update the color on movieAction component

--- src/components/watch/MovieActions.jsx
+++ src/components/watch/MovieActions.jsx
@@ -1,7 +1,12 @@
 export default function MovieActions() {
   return (
+<<<<<<< HEAD
     <div className="flex flex-wrap gap-4 pt-4">
       <button className="flex items-center gap-2 px-6 py-3 rounded-xl font-bold bg-primary hover:bg-primary/90 transition-all text-white active:scale-95">
+=======
+    <div className="flex flex-wrap gap-4 pt-4 text-white">
+      <button className="flex items-center gap-2 px-6 py-3 bg- rounded-xl font-bold bg-primary hover:bg-primary/90 text-white  transition-all active:scale-95">
+>>>>>>> 60d69a6 (Update MovieActions component styling)
         <span
           className="material-symbols-outlined"
           style={{ fontVariationSettings: '"FILL" 1' }}
@@ -10,25 +15,41 @@ export default function MovieActions() {
         </span>
         Resume Movie
       </button>
+<<<<<<< HEAD
       <button className="flex items-center gap-2 px-6 py-3 rounded-xl font-bold bg-white/10 hover:bg-white/20 border border-white/20 backdrop-blur-sm text-white transition-all">
+=======
+      <button className="flex items-center gap-2 px-6 py-3 bg-white/10 hover:bg-white/20 border border-white/20 backdrop-blur-sm rounded-xl font-bold  transition-all">
+>>>>>>> 60d69a6 (Update MovieActions component styling)
         <span className="material-symbols-outlined">add</span>
         Watchlist
       </button>
       <div className="h-12 w-px bg-white/10 mx-2"></div>
       <button
+<<<<<<< HEAD
         className="p-3 bg-white/8 hover:bg-white/20 text-primary rounded-xl hover:bg-white/5 transition-colors"
+=======
+        className="p-3 bg-white/5 text-primary rounded-xl hover:bg-white/10 transition-colors"
+>>>>>>> 60d69a6 (Update MovieActions component styling)
         title="Download"
       >
         <span className="material-symbols-outlined">download</span>
       </button>
       <button
+<<<<<<< HEAD
         className="p-3 bg-white/8 hover:bg-white/20 text-primary rounded-xl hover:bg-white/5 transition-colors"
+=======
+        className="p-3 bg-white/5 text-primary rounded-xl hover:bg-white/10 transition-colors"
+>>>>>>> 60d69a6 (Update MovieActions component styling)
         title="Share"
       >
         <span className="material-symbols-outlined">share</span>
       </button>
       <button
+<<<<<<< HEAD
         className="p-3 bg-white/8 hover:bg-white/20 text-primary rounded-xl hover:bg-white/5 transition-colors"
+=======
+        className="p-3 bg-white/5 text-primary rounded-xl hover:bg-white/10 transition-colors"
+>>>>>>> 60d69a6 (Update MovieActions component styling)
         title="Favorite"
       >
         <span className="material-symbols-outlined">favorite</span>
