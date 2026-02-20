b6588c0511c1b26eeaa335172a633af79fd32157
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-02-20 16:09:39 +0600

    Update MovieCard width and MoviesGrid layout

--- src/components/moviesPage/MoviesGrid.jsx
+++ src/components/moviesPage/MoviesGrid.jsx
@@ -1,15 +1,7 @@
 import MovieCard from "../movie/MovieCard";
 export default function MoviesGrid() {
   return (
-    <div
-      className="
-  grid
-  grid-cols-2
-  sm:[grid-template-columns:repeat(auto-fit,minmax(220px,1fr))]
-  
-  justify-items-center
-"
-    >
+    <div className="grid [grid-template-columns:repeat(auto-fit,minmax(310px,1fr))] justify-items-center ">
       <MovieCard />
       <MovieCard />
       <MovieCard />
