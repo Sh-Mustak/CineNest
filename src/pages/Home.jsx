9e05b550a261c20771db93ab592b0db61ff3b4b8
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-02-24 12:38:48 +0600

    Update Home component to use useMovieContext and pass loading/error to MovieRow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import HeroSection from "../components/movie/HeroSection";
 import MovieRow from "../components/movie/MovieRow";
-import {useContext} from "react";
-import {MovieContext} from "../context/MovieContextDefinition"
+import { useMovieContext } from "../context/useMovieContext";
 
 export default function Home() {
-  const {trending, popular} = useContext(MovieContext);
+  const { trending, popular, loading, error } = useMovieContext();
   return (
     <main className="relative">
       <HeroSection />
-      <MovieRow movies={trending} rowheader="Trending Now" />
-      <MovieRow movies={popular} rowheader="Popular Movies" />
+      <MovieRow
+        movies={trending}
+        loading={loading}
+        error={error}
+        rowheader="Trending Now"
+      />
+      <MovieRow
+        movies={popular}
+        loading={loading}
+        error={error}
+        rowheader="Popular Movies"
+      />
       <MovieRow rowheader="Top Rated" />
     </main>
   );
