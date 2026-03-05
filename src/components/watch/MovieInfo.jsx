2c404df41abd42aeb47f69dfc87031fc8d63812a
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-03-05 16:51:54 +0600

    Add poster path in MovieInfo.jsx

--- src/components/watch/MovieInfo.jsx
+++ src/components/watch/MovieInfo.jsx
@@ -2,7 +2,7 @@ import CastDetails from "./CastDetails";
 import MovieActions from "./MovieActions";
 import MovieMeta from "./MovieMeta";
 import MovieMetaGrid from "./MovieMetaGrid";
-
+import { getImageUrl } from "../../utils/helper";
 export default function MovieInfo({ movieDetails }) {
   return (
     <div className="panel act text-white" id="panel-info">
@@ -12,7 +12,7 @@ export default function MovieInfo({ movieDetails }) {
         <div>
           <div className="poster-wrap relative aspect-[2/3] rounded-xl2 overflow-hidden shadow-[0_24px_60px_rgba(0,0,0,.65),0_0_0_1px_rgba(255,255,255,.07)] max-w-[160px] sm:max-w-none">
             <img
-              src="https://lh3.googleusercontent.com/aida-public/AB6AXuASrXUaGqZ83fc9IpGN6__vouZ2ezhE7PPE8DUQlE5iusXNAjnR9Af13omCFPqc1XvfQ3Nh81sdHuhxiocFg_ouLnfWJDv0rTmy0EYcvOqRYbV6fRAsJ1JRASN4Y3hP4OPCKGN0AXo4IQlCdo0xiVRAglrU18JhLEWbfQWz3_Sf29rjLJLItt1egqx95eEti8DaohiH49oYWtsLWLtJp2qNsoex4-SIPRfzojdxdbz5Yz549_56WF9xTcF6XLbmhgO4TchOMtc0Bp2k"
+              src={getImageUrl(movieDetails.poster_path)}
               alt="Poster"
               className="w-full h-full object-cover block"
             />
