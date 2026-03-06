e746a97e9242248267a5f3863a5123e38d141d4f
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-03-06 16:34:26 +0600

    feat(MovieMetaGrid.jsx): Update MovieMetaGrid componet from api response

--- src/components/watch/MovieMetaGrid.jsx
+++ src/components/watch/MovieMetaGrid.jsx
@@ -1,13 +1,15 @@
-export default function MovieMetaGrid() {
+import { getDirector } from "../../utils/getDirector";
+
+export default function MovieMetaGrid({ movieDetails }) {
   return (
     <>
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-px bg-white/[.07] border border-white/[.07] rounded-[10px] overflow-hidden mb-4 max-w-xl">
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-px bg-white/[.07] border border-white/[.07] rounded-[10px] overflow-hidden mb-4 ">
         <div className="bg-s1 px-3 sm:px-4 py-2.5 sm:py-3 flex flex-col gap-1">
           <span className="text-[8px] sm:text-[9px] font-bold tracking-[.1em] uppercase text-white/60">
             Budget
           </span>
           <span className="text-xs sm:text-[13px] font-semibold font-mono">
-            $200M
+            {`$${movieDetails?.budget}m`}
           </span>
         </div>
         <div className="bg-s1 px-3 sm:px-4 py-2.5 sm:py-3 flex flex-col gap-1">
@@ -15,7 +17,7 @@ export default function MovieMetaGrid() {
             Revenue
           </span>
           <span className="text-xs sm:text-[13px] font-semibold font-mono text-grn">
-            $1.12B
+            {`$${movieDetails?.revenue}m`}
           </span>
         </div>
         <div className="bg-s1 px-3 sm:px-4 py-2.5 sm:py-3 flex flex-col gap-1">
@@ -23,21 +25,18 @@ export default function MovieMetaGrid() {
             Director
           </span>
           <span className="text-xs sm:text-[13px] font-semibold">
-            Elena Varkas
+            {getDirector(movieDetails)}
           </span>
         </div>
         <div className="bg-s1 px-3 sm:px-4 py-2.5 sm:py-3 flex flex-col gap-1">
           <span className="text-[8px] sm:text-[9px] font-bold tracking-[.1em] uppercase text-ctm text-white/60">
             Language
           </span>
-          <span className="text-xs sm:text-[13px] font-semibold">EN / RU</span>
+          <span className="text-xs sm:text-[13px] font-semibold">{`${movieDetails?.spoken_languages?.map((ln) => ln.iso_639_1?.toUpperCase())}`}</span>
         </div>
       </div>
       <p className="text-xs sm:text-[14px] leading-[1.75] text-[#f0ece48c] mb-5 text-justify max-w-2xl">
-        In a future where Earth's resources are depleted, a team of astronauts
-        embarks on a high-stakes mission across a wormhole in search of a new
-        home for humanity. As they navigate the unknown reaches of space and
-        time, they must confront their own fears and the secrets of the cosmos.
+        {movieDetails?.overview}
       </p>
     </>
   );
