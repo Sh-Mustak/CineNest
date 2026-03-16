256bda2a4e5babad4a607e93a57568b50006c48b
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-03-16 23:47:25 +0600

    fix: resolve VideoPlayer mobile responsive layout issue

--- src/components/watch/VideoPlayer.jsx
+++ src/components/watch/VideoPlayer.jsx
@@ -1,15 +1,13 @@
 export default function VideoPlayer({ playerUrl }) {
   if (!playerUrl) return null;
-
   return (
-    <div className="relative w-full aspect-video h-[50vh] rounded-2xl overflow-hidden  shadow-xl mb-3">
+    <div className="relative w-full aspect-video rounded-2xl overflow-hidden shadow-xl mb-3">
       <iframe
         key={playerUrl}
         src={playerUrl}
         title="Movie Player"
-        className="w-full h-full"
+        className="absolute top-0 left-0 w-full h-full"
         allowFullScreen
-        // referrerPolicy="no/-referrer"
       />
     </div>
   );
