6c0f5851e13de476757498312a85b9619201253c
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-03-18 16:55:27 +0600

    feat: implement dynamic server switching

--- src/api/embedService.js
+++
@@ -1,6 +0,0 @@
-import { embed_endpoints } from "./endpoints";
-
-export const embededService = {
-  getMoviePlayer: (id) => embed_endpoints.movie(id),
-  getTvPlayer: (id, season, episode) => embed_endpoints.tv(id, season, episode),
-};
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
-import { embededService } from "../api/embedService";
 import MovieTabs from "../components/watch/MovieTabs";
 import ServerBar from "../components/watch/ServerBar";
 import VideoPlayer from "../components/watch/VideoPlayer";
@@ -8,17 +7,21 @@ import WatchContent from "../components/watch/WatchContent";
 import { useWatchContext } from "../context/useWatchContext";
 import { useMediaDetails } from "../hooks/useMediaDetails";
 import { createWatchTabs } from "../utils/watchTabs";
+import { embed_endpoints } from "../api/endpoints";
 
 export default function Watch() {
   const { type, id } = useParams();
   const { seasonNumber, episodeNumber } = useWatchContext();
   const { data, loading, error } = useMediaDetails(type, id);
+  const [server, setServer] = useState("server1");
 
   const playerUrl = useMemo(() => {
+    const selectedServer = embed_endpoints[server];
+
     return type === "movie"
-      ? embededService.getMoviePlayer(id)
-      : embededService.getTvPlayer(id, seasonNumber, episodeNumber);
-  }, [type, id, seasonNumber, episodeNumber]);
+      ? selectedServer.movie(id)
+      : selectedServer.tv(id, seasonNumber, episodeNumber);
+  }, [type, id, seasonNumber, episodeNumber, server]);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -38,7 +41,7 @@ export default function Watch() {
   return (
     <main className="max-w-[1440px] mx-auto px-3 sm:px-5 pt-4 sm:pt-6 pb-20 min-h-screen mt-20">
       <VideoPlayer playerUrl={playerUrl} />
-      <ServerBar />
+      <ServerBar server={server} setServer={setServer} />
       <MovieTabs
         tabs={tabs}
         activeTab={activeTab}
