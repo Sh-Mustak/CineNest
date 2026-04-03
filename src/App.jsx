eff1cdebdefcfc3ff8265c99aa305b7227b05d38
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-04-03 23:46:25 +0600

    Add BottomNav component and adjust Routes layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import BottomNav from "./components/layout/BottomNav"; 
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Search from "./pages/Search";
@@ -14,16 +15,19 @@ function App() {
       <Router>
         <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/tvshows" element={<TvShows />} />
-          <Route path="/watch/:type/:id" element={<Watch />} />
-          <Route path="/search" element={<Search />} />
+        <div className="pb-16 md:pb-0"> {/* ← prevents content hiding behind bottom nav on mobile */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tvshows" element={<TvShows />} />
+            <Route path="/watch/:type/:id" element={<Watch />} />
+            <Route path="/search" element={<Search />} />
 
-          {/* <Route path="/watchlist" element={<Watchlist />} /> */}
-        </Routes>
+            {/* <Route path="/watchlist" element={<Watchlist />} /> */}
+          </Routes>
+        </div>
 
+        <BottomNav /> 
         <Footer />
       </Router>
     </>
