b99d0bf5bc7fc74142eaf41715532285266d6733
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-04-07 17:57:20 +0600

    Add crime endpoint for Spanish crime TV shows

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -17,6 +17,7 @@ export const tmdb_endpoints = {
   airingToday: `${BASE_URL}/tv/airing_today?api_key=${API_KEY}`,
   topRatedTvShows: `${BASE_URL}/tv/top_rated?api_key=${API_KEY}`,
   topRatedHindiTvShows: `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_original_language=hi&sort_by=vote_average.desc&vote_count.gte=50`,
+  crime: `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_genres=80,18&with_original_language=es&sort_by=vote_average.desc&vote_count.gte=100}`,
   seriesDetails: `${BASE_URL}/tv/{series_id}?api_key=${API_KEY}&append_to_response=credits,videos,recommendations,reviews`,
   episodes: `${BASE_URL}/tv/{series_id}/season/{season_number}?api_key=${API_KEY}`,
   discoverTv: `${BASE_URL}/discover/tv?api_key=${API_KEY}`,
