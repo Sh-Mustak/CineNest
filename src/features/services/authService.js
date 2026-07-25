f812d4b9d42a244037ad702435cb08a010dd3d77
Shahorier Mustak <shahriarmustak94@gmail.com>
2026-07-25 03:22:26 +0000

    Added partial authservice

--- src/features/services/authService.js
+++ src/features/services/authService.js
@@ -2,8 +2,20 @@ import { ID } from "appwrite";
 import {account} from "../../appwrite"
 
 class AuthService{
-    async register({name, email, password}){}
-    async login({email, password}){}
+    async register({name, email, password}){
+        return await account.create(
+            ID.unique(),
+            email,
+            password,
+            name
+        )
+    }
+    async login({email, password}){
+        return await account.createEmailPasswordSession(
+            email,
+            password
+        );
+    }
     async logout(){}
     async getCurrentUser(){}
     async sendVerification(){}
